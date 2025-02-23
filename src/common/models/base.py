d1fa965bd470fe3cdcdc6e5445b4effc452ce3bf
shapman <shapmanlv@outlook.com>
2025-02-23 23:00:34 +0800

    feat: 修改数据库实体基类

--- src/common/models/base.py
+++ src/common/models/base.py
@@ -1,41 +1,34 @@
 from sqlalchemy import Column, BigInteger, Boolean, DateTime, String
 from datetime import datetime
-import snowflake
+from ..snowflakeId import SnowflakeGenerator
 
 
 class Entity:
-    id = Column(BigInteger, primary_key=True, default=snowflake.generate_id)
+    id = Column(BigInteger, primary_key=True, default=SnowflakeGenerator().generate())
 
     @staticmethod
     def generate_snowflake_id():
-        return snowflake.generate_id()
+        return SnowflakeGenerator().generate()
 
 
 class SoftDeleteBase:
-    """��ɾ������"""
-
-    deleted = Column(Boolean, default=False, nullable=False, comment="�Ƿ�ɾ��")
-    deleted_at = Column(DateTime, nullable=True, comment="ɾ��ʱ��")
+    deleted = Column(Boolean, default=False, nullable=False, comment="是否已删除")
+    deleted_at = Column(DateTime, nullable=True, comment="删除时间")
 
     def soft_delete(self):
-        """ִ����ɾ������"""
         self.deleted = True
-        self.deleted_at = datetime.now()
+        self.deleted_at = datetime.now(datetime.timezone.utc)
 
 
 class AuditBase:
-    """��ƻ���"""
+    created_by = Column(String(50), comment="创建者")
+    created_at = Column(DateTime, default=datetime.now, comment="创建时间")
+    updated_by = Column(String(50), comment="修改者")
+    updated_at = Column(DateTime, onupdate=datetime.now, comment="修改时间")
 
-    created_by = Column(String(50), comment="������")
-    created_at = Column(DateTime, default=datetime.now, comment="����ʱ��")
-    updated_by = Column(String(50), comment="�޸���")
-    updated_at = Column(DateTime, onupdate=datetime.now, comment="�޸�ʱ��")
-
-    def set_creator(self, user_id: str):
-        """��ʼ����������Ϣ"""
+    def set_creator(self, user_id: int):
         self.created_by = user_id
         self.updated_by = user_id
 
-    def set_updater(self, user_id: str):
-        """�����޸�����Ϣ"""
+    def set_updater(self, user_id: int):
         self.updated_by = user_id
