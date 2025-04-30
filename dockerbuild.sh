d1156414c44b7aba1caf483b6c9079d1fe391838
shapman <shapmanlv@outlook.com>
2025-04-30 16:54:26 +0800

    feat: Modify the nginx image build

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,3 +1,3 @@
-docker build -t deepagenix-nginx-consul-template -f ./src/gateway/nginx-consul-template .
+docker build -t deepagenix-nginx-upsync -f ./src/gateway/nginx-upsync/Dockerfile .
 docker build -t deepagenix-component-python -f ./src/components/python/Dockerfile .
 docker build -t deepagenix-core -f ./src/core/Dockerfile .
