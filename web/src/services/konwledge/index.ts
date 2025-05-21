c2c56d049a0ebf36925e4988850170d94385562e
damon <894316189@qq.com>
2025-05-21 23:02:02 +0800

    Refactors knowledge management data structure
    Refactors the knowledge management feature to align with backend API changes.
    
    - Moves schema definitions and mock data to the `services/konwledge` directory to reflect the data structures used by the API.
    - Updates the knowledge settings form to use the new schema and integrates with the `useCreateKnowledge` and `useUpdateKnowledge` mutations.
    - Adjusts the knowledge list component to fetch data from the API using `useGetKnowledges`.

--- web/src/services/konwledge/index.ts
+++ web/src/services/konwledge/index.ts
@@ -1,5 +1,8 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMemo } from 'react'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { queryClient } from '@/main'
 import { httpClient } from '@/lib/http-client'
+import { FormKnowledgeItem, KnowledgeItem } from './schema'
 
 export interface LanguagesItem {
   name: string
@@ -36,4 +39,56 @@ export const useGetPlugins = () => {
   })
 
   return { data, isLoading }
+}
+
+export interface IndexConfig {
+  documentSegmentModel: string
+  embeddingModel: string
+  participlePlugin: string
+}
+
+export const useGetKnowledges = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-knowledges'],
+    queryFn: () => {
+      return httpClient.get<KnowledgeItem[]>(`/dk/api/knowledge`)
+    },
+  })
+
+  const knowledges = useMemo(() => {
+    return data?.data ?? []
+  }, [data])
+
+  return { knowledges, isLoading }
+}
+
+export const useGetKnowledgeDetail = (params: { id: string }) => {
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-knowledge-detail', params],
+    queryFn: () => {
+      return httpClient.get<KnowledgeItem[]>(`/dk/api/knowledge/${params.id}`)
+    },
+  })
+
+  return { data, isLoading }
+}
+
+export function useCreateKnowledge() {
+  return useMutation({
+    mutationFn: (body: FormKnowledgeItem) =>
+      httpClient.post(`/dk/api/knowledge`, body),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['get-knowledges'] })
+    },
+  })
+}
+
+export function useUpdateKnowledge() {
+  return useMutation({
+    mutationFn: (body: KnowledgeItem) =>
+      httpClient.put(`/dk/api/knowledge/${body.id}`, body),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['get-knowledge-detail'] })
+    },
+  })
 }
